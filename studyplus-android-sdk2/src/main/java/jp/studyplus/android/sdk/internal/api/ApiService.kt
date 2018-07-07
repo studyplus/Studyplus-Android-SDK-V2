1b446eb9a52ea1ae6ca68f800cfb7b00a6dd1351
木下照章 <tkinoshita@groove-systems.com>
2018-07-08 05:20:22 +0900

    実装完了

---
+++ studyplus-android-sdk2/src/main/java/jp/studyplus/android/sdk/internal/api/ApiService.kt
@@ -0,0 +1,22 @@
+package jp.studyplus.android.sdk.internal.api
+
+import io.reactivex.Observable
+import jp.studyplus.android.sdk.internal.api.response.PostStudyRecordsResponse
+import jp.studyplus.android.sdk.record.StudyRecord
+import okhttp3.Response
+import okhttp3.ResponseBody
+import retrofit2.http.*
+
+interface ApiService {
+    @Headers(value = [
+        "Accept: application/json",
+        "Content-type: application/json"
+    ])
+    @POST("/v1/study_records")
+    fun postStudyRecords(
+            // TODO:https://studyplus.slack.com/archives/C0HD1DLM9/p1530504393000126
+            // ｀HTTP_AUTHORIZATION｀ → ｀Authorization｀
+            @Header("Authorization") oauth: String,
+            @Body studyRecord: StudyRecord)
+            : Observable<PostStudyRecordsResponse>
+}
