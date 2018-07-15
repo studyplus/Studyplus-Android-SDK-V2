4889fa95435f8cb2b0f04b35375868438becadc3
木下照章 <tkinoshita@groove-systems.com>
2018-07-16 07:49:01 +0900

    不要なコメント削除

--- studyplus-android-sdk2/src/main/java/jp/studyplus/android/sdk/internal/api/ApiService.kt
+++ studyplus-android-sdk2/src/main/java/jp/studyplus/android/sdk/internal/api/ApiService.kt
@@ -12,8 +12,6 @@ interface ApiService {
     ])
     @POST("/v1/study_records")
     fun postStudyRecords(
-            // TODO:https://studyplus.slack.com/archives/C0HD1DLM9/p1530504393000126
-            // ｀HTTP_AUTHORIZATION｀ → ｀Authorization｀
             @Header("Authorization") oauth: String,
             @Body studyRecord: StudyRecord)
             : Observable<PostStudyRecordsResponse>
